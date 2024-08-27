a57de713f4722556b5666ea9979ef73764c6b1b2
Onii-Chan <140295418+waqarbaig6133@users.noreply.github.com>
2024-08-27 08:32:00 -0400

    Update TransparencyKitty.bash

--- TransparencyKitty.bash
+++ TransparencyKitty.bash
@@ -1,5 +1,7 @@
-//Go on the terminal and enter this command to open editing mode
+#Go on the terminal and enter this command to open editing mode
 nano ~/.config/kitty/kitty.conf
 
-//Then change the background colors
-background_opacity 0.0-1.0
+#Then change the background colors
+background_opacity 0.0-1.0 #Can pick opacity range between 0.0 and 0.1
+
+#This command works with the kitty terminal. The regular terminal can be changed in the settings
